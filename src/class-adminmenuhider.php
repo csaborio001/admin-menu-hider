843ff31139fb5d4a502a75c1eeb085ac8c8100ab
csaborio <christian@saborio.org>
2020-06-05 12:30:32 +1000

    everything broken

---
+++ src/class-adminmenuhider.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test
+ */
+
+namespace Cows;
+
+class AdminMenuHider {
+	public function __construct($some_cows) {
+		echo 'cows';
+	}
+
+	public function change_greeting_message() {
+		echo 'fuck off';
+	}
+}
